08286eca4de462417ef5c2d01748f671e370d9e4
sangkny <sangkny@yahoo.com>
2019-02-28 19:45:46 +0900

    2019-02-28-19:45 MFC_APP added

---
+++ utils/ITMS_MFC/ITMS_MFCDlg.h
@@ -0,0 +1,41 @@
+
+// ITMS_MFCDlg.h : 헤더 파일
+//
+
+#pragma once
+#include "afxwin.h"
+
+
+// CITMS_MFCDlg 대화 상자
+class CITMS_MFCDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CITMS_MFCDlg(CWnd* pParent = NULL);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ITMS_MFC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CStatic m_picture;
+
+	/*VideoCapture *capture;
+	Mat mat_frame;
+	CImage cimage_mfc;*/
+};
