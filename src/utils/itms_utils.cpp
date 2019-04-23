7c5d406e342b5824b139b5b03240ec3c78cd3bbc
sangkny <sangkny@yahoo.com>
2019-04-23 14:32:01 +0900

    2019-04-23-14:23

--- src/utils/itms_utils.cpp
+++ src/utils/itms_utils.cpp
@@ -4016,5 +4016,489 @@ namespace itms {
 	  return (this->itmsres->objSpeed);
   }
   ;
+  // car counting related function implementation
+  ///
+  /// \brief CarsCounting::CarsCounting
+  /// \param parser
+  ///
+  CarsCounting::CarsCounting(const cv::CommandLineParser& parser)
+	  :
+	  m_showLogs(true),
+	  m_fps(25),
+	  m_useLocalTracking(false), // local tracking control
+	  m_isTrackerInitialized(false),
+	  m_startFrame(0),
+	  m_endFrame(0),
+	  m_finishDelay(0)
+  {
+	  m_inFile = parser.get<std::string>(0);
+	  m_outFile = parser.get<std::string>("out");
+	  m_showLogs = parser.get<int>("show_logs") != 0;
+	  m_startFrame = parser.get<int>("start_frame");
+	  m_endFrame = parser.get<int>("end_frame");
+	  m_finishDelay = parser.get<int>("end_delay");
+
+	  m_colors.push_back(cv::Scalar(255, 0, 0));
+	  m_colors.push_back(cv::Scalar(0, 255, 0));
+	  m_colors.push_back(cv::Scalar(0, 0, 255));
+	  m_colors.push_back(cv::Scalar(255, 255, 0));
+	  m_colors.push_back(cv::Scalar(0, 255, 255));
+	  m_colors.push_back(cv::Scalar(255, 0, 255));
+	  m_colors.push_back(cv::Scalar(255, 127, 255));
+	  m_colors.push_back(cv::Scalar(127, 0, 255));
+	  m_colors.push_back(cv::Scalar(127, 0, 127));
+  }
+
+  ///
+  /// \brief CarsCounting::~CarsCounting
+  ///
+  CarsCounting::~CarsCounting()
+  {
+	  /*if (m_detector) {
+	  m_detector.release();
+	  m_detector = nullptr;
+	  }
+	  if (m_tracker) {
+	  m_tracker.release();
+	  m_tracker = nullptr;
+	  }*/
+  }
+
+  ///
+  /// \brief CarsCounting::Process
+  ///
+  void CarsCounting::Process()
+  {
+	  cv::VideoWriter writer;
+
+	  cv::namedWindow("Video", cv::WINDOW_NORMAL | cv::WINDOW_KEEPRATIO);
+
+	  int k = 0;
+
+	  double freq = cv::getTickFrequency();
+
+	  int64 allTime = 0;
+
+	  bool manualMode = false;
+	  int framesCounter = m_startFrame + 1;
+
+	  cv::VideoCapture capture;
+	  if (m_inFile.size() == 1)
+	  {
+		  capture.open(atoi(m_inFile.c_str()));
+	  }
+	  else
+	  {
+		  capture.open(m_inFile);
+	  }
+	  if (!capture.isOpened())
+	  {
+		  std::cerr << "Can't open " << m_inFile << std::endl;
+		  return;
+	  }
+	  capture.set(cv::CAP_PROP_POS_FRAMES, m_startFrame);
+
+	  m_fps = std::max(1.f, (float)capture.get(cv::CAP_PROP_FPS));
+
+	  //m_fps = std::max(1.f, (float)capture.get(cv::CAP_PROP_FPS)); // is it required for 2 times ?
+
+	  float scaleFactor = 0.5;
+	  using namespace cv;
+	  std::vector<cv::Point> road_roi_pts;
+	  std::vector<std::vector<cv::Point>> Road_ROI_Pts; // sidewalks and carlanes
+
+														//20180912_112338
+														// side walk1
+	  road_roi_pts.push_back(Point(932.75, 100.25)*scaleFactor);
+	  road_roi_pts.push_back(Point(952.25, 106.25)*scaleFactor);
+	  road_roi_pts.push_back(Point(434.75, 1055.75)*scaleFactor);
+	  road_roi_pts.push_back(Point(235.25, 1054.25)*scaleFactor);
+	  Road_ROI_Pts.push_back(road_roi_pts);
+	  road_roi_pts.clear();
+	  // car lane
+	  road_roi_pts.push_back(Point(949.25, 104.75)*scaleFactor);
+	  road_roi_pts.push_back(Point(1015.25, 103.25)*scaleFactor);
+	  road_roi_pts.push_back(Point(1105.25, 1048.25)*scaleFactor);
+	  road_roi_pts.push_back(Point(416.75, 1057.25)*scaleFactor);
+	  Road_ROI_Pts.push_back(road_roi_pts);
+	  road_roi_pts.clear();
+	  // side walk2
+	  road_roi_pts.push_back(Point(1009.25, 101.75)*scaleFactor);
+	  road_roi_pts.push_back(Point(1045.25, 98.75)*scaleFactor);
+	  road_roi_pts.push_back(Point(1397.75, 1052.75)*scaleFactor);
+	  road_roi_pts.push_back(Point(1087.25, 1049.75)*scaleFactor);
+	  Road_ROI_Pts.push_back(road_roi_pts);
+	  road_roi_pts.clear();
+
+
+	  cv::Mat colorFrame;
+	  cv::UMat grayFrame;
+
+	  cv::Mat road_mask;
+
+	  for (;;)
+	  {
+		  capture >> colorFrame;
+		  if (colorFrame.empty())
+		  {
+			  std::cerr << "Frame is empty!" << std::endl;
+			  break;
+		  }
+		  resize(colorFrame, colorFrame, cv::Size(), scaleFactor, scaleFactor);
+		  cv::cvtColor(colorFrame, grayFrame, cv::COLOR_BGR2GRAY);
+		  if (framesCounter == m_startFrame + 1) {
+			  // road mask generation
+			  road_mask = cv::Mat::zeros(grayFrame.size(), grayFrame.type());
+			  for (int ir = 0; ir < Road_ROI_Pts.size(); ir++)
+				  fillConvexPoly(road_mask, Road_ROI_Pts.at(ir).data(), Road_ROI_Pts.at(ir).size(), Scalar(255, 255, 255), 8);
+
+			  if (road_mask.channels() > 1)
+				  cvtColor(road_mask, road_mask, CV_BGR2GRAY);
+			  if (0) {
+				  imshow("road mask", road_mask);
+				  waitKey(1);
+			  }
+		  }
+		  if (!road_mask.empty()) // only one time setting
+			  bitwise_and(road_mask, grayFrame, grayFrame);
+
+		  if (!m_isTrackerInitialized)
+		  {
+			  m_isTrackerInitialized = InitTracker(grayFrame);
+			  if (!m_isTrackerInitialized)
+			  {
+				  std::cerr << "Tracker initilize error!!!" << std::endl;
+				  break;
+			  }
+		  }
+
+		  if (!writer.isOpened())
+		  {
+			  writer.open(m_outFile, cv::VideoWriter::fourcc('H', 'F', 'Y', 'U'), m_fps, colorFrame.size(), true);
+		  }
+
+		  int64 t1 = cv::getTickCount();
+
+		  cv::UMat clFrame;
+		  if (!GrayProcessing() || !m_tracker->GrayFrameToTrack())
+		  {
+			  clFrame = colorFrame.getUMat(cv::ACCESS_READ);
+		  }
+
+		  m_detector->Detect(GrayProcessing() ? grayFrame : clFrame);
+
+		  const regions_t& regions = m_detector->GetDetects();
+
+		  m_tracker->Update(regions, m_tracker->GrayFrameToTrack() ? grayFrame : clFrame, m_fps);
+
+
+		  int64 t2 = cv::getTickCount();
+
+		  allTime += t2 - t1;
+		  int currTime = cvRound(1000 * (t2 - t1) / freq);
+
+		  DrawData(colorFrame, framesCounter, currTime);
+
+		  cv::imshow("Video", colorFrame);
+
+		  int waitTime = manualMode ? 0 : std::max<int>(1, cvRound(1000 / m_fps - currTime));
+		  k = cv::waitKey(waitTime);
+		  if (k == 'm' || k == 'M')
+		  {
+			  manualMode = !manualMode;
+		  }
+		  else if (k == 27)
+		  {
+			  break;
+		  }
+
+		  if (writer.isOpened())
+		  {
+			  writer << colorFrame;
+		  }
+
+		  ++framesCounter;
+		  if (m_endFrame && framesCounter > m_endFrame)
+		  {
+			  std::cout << "Process: riched last " << m_endFrame << " frame" << std::endl;
+			  break;
+		  }
+	  }
+
+	  std::cout << "work time = " << (allTime / freq) << std::endl;
+	  cv::waitKey(m_finishDelay);
+  }
+
+  ///
+  /// \brief CarsCounting::GrayProcessing
+  /// \return
+  ///
+  bool CarsCounting::GrayProcessing() const
+  {
+	  return true;
+  }
+
+  ///
+  /// \brief CarsCounting::DrawTrack
+  /// \param frame
+  /// \param resizeCoeff
+  /// \param track
+  /// \param drawTrajectory
+  /// \param isStatic
+  ///
+  void CarsCounting::DrawTrack(cv::Mat frame,
+	  int resizeCoeff,
+	  const CTrack& track,
+	  bool drawTrajectory,
+	  bool isStatic
+  )
+  {
+	  auto ResizeRect = [&](const cv::Rect& r) -> cv::Rect
+	  {
+		  return cv::Rect(resizeCoeff * r.x, resizeCoeff * r.y, resizeCoeff * r.width, resizeCoeff * r.height);
+	  };
+	  auto ResizePoint = [&](const cv::Point& pt) -> cv::Point
+	  {
+		  return cv::Point(resizeCoeff * pt.x, resizeCoeff * pt.y);
+	  };
+
+	  if (isStatic)
+	  {
+		  std::ostringstream os;
+		  os << int(track.m_trackID);
+#if (CV_VERSION_MAJOR >= 4)
+		  cv::rectangle(frame, ResizeRect(track.GetLastRect()), cv::Scalar(255, 0, 255), 2, cv::LINE_AA);
+		  putText(frame, "s: " + os.str(), cv::Point(ResizeRect(track.GetLastRect()).x, ResizeRect(track.GetLastRect()).y - 1), cv::FONT_HERSHEY_SIMPLEX,
+			  0.5, cv::Scalar(255, 0, 255), 1);
+#else
+		  cv::rectangle(frame, ResizeRect(track.GetLastRect()), cv::Scalar(255, 0, 255), 2, CV_AA);
+		  putText(frame, "s: " + os.str(), cv::Point(ResizeRect(track.GetLastRect()).x, ResizeRect(track.GetLastRect()).y - 1), cv::FONT_HERSHEY_SIMPLEX,
+			  0.5, cv::Scalar(255, 0, 255), 1);
+#endif
+	  }
+	  else
+	  {
+		  std::ostringstream os;
+		  os << int(track.m_trackID);
+#if (CV_VERSION_MAJOR >= 4)
+		  cv::rectangle(frame, ResizeRect(track.GetLastRect()), cv::Scalar(0, 255, 0), 1, cv::LINE_AA);
+		  putText(frame, "a: " + os.str(), cv::Point(ResizeRect(track.GetLastRect()).x, ResizeRect(track.GetLastRect()).y - 1), cv::FONT_HERSHEY_SIMPLEX,
+			  0.5, cv::Scalar(0, 255, 0), 1);
+#else
+		  cv::rectangle(frame, ResizeRect(track.GetLastRect()), cv::Scalar(0, 255, 0), 1, CV_AA);
+
+		  putText(frame, "a: " + os.str(), cv::Point(ResizeRect(track.GetLastRect()).x, ResizeRect(track.GetLastRect()).y - 1), cv::FONT_HERSHEY_SIMPLEX,
+			  0.5, cv::Scalar(0, 255, 0), 1);
+#endif
+	  }
+
+	  if (drawTrajectory)
+	  {
+		  cv::Scalar cl = m_colors[track.m_trackID % m_colors.size()];
+
+		  for (size_t j = 0; j < track.m_trace.size() - 1; ++j)
+		  {
+			  const TrajectoryPoint& pt1 = track.m_trace.at(j);
+			  const TrajectoryPoint& pt2 = track.m_trace.at(j + 1);
+#if (CV_VERSION_MAJOR >= 4)
+			  cv::line(frame, ResizePoint(pt1.m_prediction), ResizePoint(pt2.m_prediction), cl, 1, cv::LINE_AA);
+#else
+			  cv::line(frame, ResizePoint(pt1.m_prediction), ResizePoint(pt2.m_prediction), cl, 1, CV_AA);
+#endif
+			  if (!pt2.m_hasRaw)
+			  {
+#if (CV_VERSION_MAJOR >= 4)
+				  cv::circle(frame, ResizePoint(pt2.m_prediction), 4, cl, 1, cv::LINE_AA);
+#else
+				  cv::circle(frame, ResizePoint(pt2.m_prediction), 4, cl, 1, CV_AA);
+#endif
+			  }
+		  }
+	  }
+
+	  if (m_useLocalTracking)
+	  {
+		  cv::Scalar cl = m_colors[track.m_trackID % m_colors.size()];
+
+		  for (auto pt : track.m_lastRegion.m_points)
+		  {
+#if (CV_VERSION_MAJOR >= 4)
+			  cv::circle(frame, cv::Point(cvRound(pt.x), cvRound(pt.y)), 1, cl, -1, cv::LINE_AA);
+#else
+			  cv::circle(frame, cv::Point(cvRound(pt.x), cvRound(pt.y)), 1, cl, -1, CV_AA);
+#endif
+		  }
+	  }
+  }
+
+  ///
+  /// \brief CarsCounting::InitTracker
+  /// \param grayFrame
+  ///
+  bool CarsCounting::InitTracker(cv::UMat frame)
+  {
+	  m_minObjWidth = 5;//frame.cols / 50;
+
+	  const int minStaticTime = 2;
+
+	  config_t config;
+#if 1
+	  config["history"] = std::to_string(cvRound(10 * minStaticTime * m_fps));
+	  config["varThreshold"] = "16";
+	  config["detectShadows"] = "1";
+	  m_detector = std::unique_ptr<BaseDetector>(CreateDetector(tracking::Detectors::Motion_MOG2, config, m_useLocalTracking, frame));
+#else
+	  config["minPixelStability"] = "15";
+	  config["maxPixelStability"] = "900";
+	  config["useHistory"] = "1";
+	  config["isParallel"] = "1";
+	  m_detector = std::unique_ptr<BaseDetector>(CreateDetector(tracking::Detectors::Motion_CNT, config, m_useLocalTracking, frame));
+#endif
+	  m_detector->SetMinObjectSize(cv::Size(m_minObjWidth, m_minObjWidth));
+
+	  TrackerSettings settings;
+	  settings.m_useLocalTracking = m_useLocalTracking;
+	  settings.m_distType = tracking::DistCenters;
+	  settings.m_kalmanType = tracking::KalmanLinear;
+	  settings.m_filterGoal = tracking::FilterRect;
+	  settings.m_lostTrackType = tracking::TrackKCF;// original TrackerCSRT; // Use KCF tracker for collisions resolving
+	  settings.m_matchType = tracking::MatchHungrian;
+	  settings.m_dt = 0.5f;                             // Delta time for Kalman filter
+	  settings.m_accelNoiseMag = 0.5f;                  // Accel noise magnitude for Kalman filter
+	  settings.m_distThres = frame.rows / 15.f;         // Distance threshold between region and object on two frames
+
+	  settings.m_useAbandonedDetection = true;		 // check the given region is static or not (abandoned)
+	  if (settings.m_useAbandonedDetection)
+	  {
+		  settings.m_minStaticTime = minStaticTime;
+		  settings.m_maxStaticTime = 6;
+		  settings.m_maximumAllowedSkippedFrames = cvRound(settings.m_minStaticTime * m_fps); // Maximum allowed skipped frames
+		  settings.m_maxTraceLength = 2 * settings.m_maximumAllowedSkippedFrames;        // Maximum trace length
+	  }
+	  else
+	  {
+		  settings.m_maximumAllowedSkippedFrames = cvRound(2 * m_fps); // Maximum allowed skipped frames
+		  settings.m_maxTraceLength = cvRound(4 * m_fps);              // Maximum trace length
+	  }
+
+	  m_tracker = std::make_unique<CTracker>(settings);
+
+	  return true;
+  }
+
+  ///
+  /// \brief CarsCounting::DrawData
+  /// \param frame
+  ///
+  void CarsCounting::DrawData(cv::Mat frame, int framesCounter, int currTime)
+  {
+	  if (m_showLogs)
+	  {
+		  std::cout << "Frame " << framesCounter << ": tracks = " << m_tracker->tracks.size() << ", time = " << currTime << std::endl;
+	  }
+
+	  std::set<size_t> currIntersections;
+
+	  for (const auto& track : m_tracker->tracks)
+	  {
+		  if (track->IsStatic())
+		  {
+			  DrawTrack(frame, 1, *track, true, true);
+		  }
+		  else
+		  {
+			  if (track->IsRobust(cvRound(m_fps / 4),          // Minimal trajectory size
+				  0.7f,                        // Minimal ratio raw_trajectory_points / trajectory_lenght
+				  cv::Size2f(0.1f, 8.0f))      // Min and max ratio: width / height
+				  )
+			  {
+				  DrawTrack(frame, 1, *track, true);
+
+				  CheckLinesIntersection(*track, static_cast<float>(frame.cols), static_cast<float>(frame.rows), currIntersections);
+			  }
+		  }
+	  }
+
+	  m_lastIntersections.clear();
+	  m_lastIntersections = currIntersections;
+
+	  //m_detector->CalcMotionMap(frame);
+
+	  for (const auto& rl : m_lines)
+	  {
+		  rl.Draw(frame);
+	  }
+  }
+
+  ///
+  /// \brief CarsCounting::AddLine
+  /// \param newLine
+  ///
+  void CarsCounting::AddLine(const RoadLine& newLine)
+  {
+	  m_lines.push_back(newLine);
+  }
+
+  ///
+  /// \brief CarsCounting::GetLine
+  /// \param lineUid
+  /// \return
+  ///
+  bool CarsCounting::GetLine(unsigned int lineUid, RoadLine& line)
+  {
+	  for (const auto& rl : m_lines)
+	  {
+		  if (rl.m_uid == lineUid)
+		  {
+			  line = rl;
+			  return true;
+		  }
+	  }
+	  return false;
+  }
+
+  ///
+  /// \brief CarsCounting::RemoveLine
+  /// \param lineUid
+  /// \return
+  ///
+  bool CarsCounting::RemoveLine(unsigned int lineUid)
+  {
+	  for (auto it = std::begin(m_lines); it != std::end(m_lines);)
+	  {
+		  if (it->m_uid == lineUid)
+		  {
+			  it = m_lines.erase(it);
+		  }
+		  else
+		  {
+			  ++it;
+		  }
+	  }
+	  return false;
+  }
+
+  ///
+  /// \brief CarsCounting::CheckLinesIntersection
+  /// \param track
+  ///
+  void CarsCounting::CheckLinesIntersection(const CTrack& track, float xMax, float yMax, std::set<size_t>& currIntersections)
+  {
+	  auto Pti2f = [&](cv::Point pt) -> cv::Point2f
+	  {
+		  return cv::Point2f(pt.x / xMax, pt.y / yMax);
+	  };
+
+	  for (auto& rl : m_lines)
+	  {
+		  if (m_lastIntersections.find(track.m_trackID) == m_lastIntersections.end())
+		  {
+			  if (rl.IsIntersect(Pti2f(track.m_trace[track.m_trace.size() - 3]), Pti2f(track.m_trace[track.m_trace.size() - 1])))
+			  {
+				  currIntersections.insert(track.m_trackID);
+			  }
+		  }
+	  }
+  }
 
 } // itms namespace
