59e840bf6d4037c595f60e9fcb10c13b3aa7d7b3
sangkny <sangkny@yahoo.com>
2019-04-23 14:33:21 +0900

    2019-04-23-14:33

---
+++ src/detector/BaseDetector.cpp
@@ -0,0 +1,37 @@
+#include "./detector/BaseDetector.h"
+#include "./detector/MotionDetector.h"
+
+///
+/// \brief CreateDetector
+/// \param detectorType
+/// \param collectPoints
+/// \param gray
+/// \return
+///
+BaseDetector* CreateDetector(
+        tracking::Detectors detectorType,
+        const config_t& config,
+        bool collectPoints,
+        cv::UMat& gray
+        )
+{
+    BaseDetector* detector = nullptr;
+
+    switch (detectorType)
+    {
+    case tracking::Motion_MOG2:
+        detector = new MotionDetector(BackgroundSubtract::BGFG_ALGS::ALG_MOG2, collectPoints, gray);
+        break;
+    
+
+    default:
+        break;
+    }
+
+    if (!detector->Init(config))
+    {
+        delete detector;
+        detector = nullptr;
+    }
+    return detector;
+}
